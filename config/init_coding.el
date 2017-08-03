bb6ac6822d26f7ab8bb2d0aa2846c9ba785903bf
Salvador Ruiz <salvador.ruiz.r@gmail.com>
2017-08-04 00:00:44 +0200

    Beginning to customize python environment

---
+++ config/init_coding.el
@@ -0,0 +1,25 @@
+; activate all the packages (in particular autoloads)
+
+
+; assure to install jedi, rope, flake8, importmagic
+(use-package elpy
+  :ensure t
+  :defer 2
+  :config
+  (progn
+    ;; Use Flycheck instead of Flymake
+    (when (require 'flycheck nil t)
+      (remove-hook 'elpy-modules 'elpy-module-flymake)
+      (remove-hook 'elpy-modules 'elpy-module-yasnippet)
+      (remove-hook 'elpy-mode-hook 'elpy-module-highlight-indentation)
+      (add-hook 'elpy-mode-hook 'flycheck-mode)
+      )
+    (elpy-enable)
+    ;; jedi is great
+    (setq elpy-rpc-backend "jedi")
+    )
+  )
+
+(use-package magit)
+(use-package projectile)
+(projectile-global-mode)
--- init.el
+++ init.el
@@ -36,6 +36,7 @@
 
 (load "~/.emacs.d/config/init_org.el")
 (load "~/.emacs.d/config/init_mail.el")
+(load "~/.emacs.d/config/init_coding.el")
 
 ;; SETTING LOCALE UTF-8
 (setq locale-coding-system 'utf-8)
@@ -91,10 +92,6 @@
 
 (global-set-key (kbd "s-M") 'hydra-modes/body)
 
-
-; activate all the packages (in particular autoloads)
-(use-package elpy)
-
 (use-package multiple-cursors)
 
 ; load theme
@@ -104,7 +101,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (elpy)))
+;; '(package-selected-packages (quote (projectile magit elpy)))
  '(send-mail-function (quote smtpmail-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

b3d95b6fd4da3ae7f6609150c3a5c704c56fb93c
Salvador Ruiz <salvador.ruiz.r@gmail.com>
2017-08-05 18:05:02 +0200

    Removed double loading of packages

--- init.el
+++ init.el
@@ -101,7 +101,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-;; '(package-selected-packages (quote (projectile magit elpy)))
+ '(package-selected-packages
+   (quote
+    (magit use-package transpose-frame projectile org-bullets multiple-cursors hydra elpy dash)))
  '(send-mail-function (quote smtpmail-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

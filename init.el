7afecc45cb14893fe554d555c769cea64acf12f6
sruizr <sruizr@gmail.com>
2017-08-06 13:13:42 +0200

    Ni idea

--- init.el
+++ init.el
@@ -101,7 +101,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-;; '(package-selected-packages (quote (projectile magit elpy)))
+ '(package-selected-packages
+   (quote
+    (multiple-cursors transpose-frame hydra projectile magit elpy use-package org-bullets)))
  '(send-mail-function (quote smtpmail-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

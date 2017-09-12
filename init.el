efc75d726fecfad5de8e546b5b93c8e69007371f
Salvador Ruiz <salvador.ruiz.r@gmail.com>
2017-09-12 23:17:02 +0200

    Modified init.el about?

--- init.el
+++ init.el
@@ -104,7 +104,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (elpy)))
+ '(package-selected-packages (quote (hydra elpy)))
  '(send-mail-function (quote smtpmail-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

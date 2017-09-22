839dd0a77acd58b4eaacb153f34423dfcf00b865
sruizr <sruizr@gmail.com>
2017-09-22 18:45:16 +0200

    Python mode is hooked to linum

--- init.el
+++ init.el
@@ -176,10 +176,42 @@ _SPC_ cancel	_o_nly this   	_d_elete
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#eaeaea" "#d54e53" "#b9ca4a" "#e7c547" "#7aa6da" "#c397d8" "#70c0b1" "#424242"))
+ '(custom-safe-themes
+   (quote
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+ '(fci-rule-color "#424242")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(package-hidden-regexps (quote ("helm-projectile")))
  '(package-selected-packages
    (quote
-    (helm-projectile hydra magit use-package transpose-frame projectile org-bullets multiple-cursors hydra elpy dash)))
- '(send-mail-function (quote smtpmail-send-it)))
+    (color-theme-sanityinc-tomorrow helm-projectile hydra magit use-package transpose-frame projectile org-bullets multiple-cursors hydra elpy dash)))
+ '(send-mail-function (quote smtpmail-send-it))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#d54e53")
+     (40 . "#e78c45")
+     (60 . "#e7c547")
+     (80 . "#b9ca4a")
+     (100 . "#70c0b1")
+     (120 . "#7aa6da")
+     (140 . "#c397d8")
+     (160 . "#d54e53")
+     (180 . "#e78c45")
+     (200 . "#e7c547")
+     (220 . "#b9ca4a")
+     (240 . "#70c0b1")
+     (260 . "#7aa6da")
+     (280 . "#c397d8")
+     (300 . "#d54e53")
+     (320 . "#e78c45")
+     (340 . "#e7c547")
+     (360 . "#b9ca4a"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -202,10 +234,9 @@ _SPC_ cancel	_o_nly this   	_d_elete
   (forward-line -1)
   (indent-according-to-mode))
 
-
 (delete-selection-mode 1)
-
-
+(add-hook 'python-mode 'linum-mode)
+(global hl-line-mode)
 ;; Key bindings for multiple cursors
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
